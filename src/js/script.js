315e8146abd6ac456a54deafbf25e31d90249119
ben980 <ben980@ben980.com>
2017-12-22 22:59:21 +0000

    SAVE

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,8 @@ Js Scirpt for Data visualisation Projet : Evolution of Rock Music
 Autor : Benjamin Girard / Paul Feng / Olga Popovych
 
 
+Mains Script : It load the json , manage data , and manange the automatic timeline
+
 */
 
 //Class for benchmark !
@@ -19,20 +21,23 @@ var timer = function(name) {
     }
 };
 //Class for benchmark !
+
+
+//List of all Genre with theire associated color/icone that we use in the projet
 //"psychedelic rock","progressive rock" ,'#33a02c' - 4
 const GENRES = ["alternative rock","blues-rock","glam rock","indie rock","pop rock", "post rock", "psychedelic rock","rock 'n roll", "soft rock"]//["rock 'n roll","pop rock","blues-rock","indie rock","soft rock","alternative rock","hard rock"].sort();//...TODO
 const COLOR = ['#e31a1c','#6a51a3','#800026','#fd8d3c','#ff6600','#df65b0','#7a0177','#3690c0','#9e9ac8']
 //['#a6cee3','#1f78b4','#b2df8a','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a']//["#d53e4f", "#fc8d59", "#fee08b", "#ffffbf", "#e6f598", "#99d594", "#3288bd"] //[ "#547980" ,"#355C7D","#9DE0AD","#EDE574","#594F4F","#F8B195","#86AF49" ]
 const ICONE_NAMES = ["guitar","guitare", "stars", "music-note", "star","bus","rocker","rock-and-roll","headphones"]
 
-var ICONES = [];
+
 //First year of studie
 const FIRSTYEAR = 1956;
 //Last years of studie
 const LASTYEAR = 2010;
 //
 
-//Select what year show on the maps:
+//Select what year are selected by the user:
 var YEARSTART = 0;
 var YEAREND = 2900;
 
@@ -40,27 +45,24 @@ var YEAREND = 2900;
 var SELECTEDGENRE = GENRES.sort();
 //ALL the DATA
 var DATA = [];
-//MAP INIT:
+//List of icone : array fill once at start up
+var ICONES = [];
 
 
 
-// GENRE SLECTION:
+// GENRE SLECTION bar:
+// for in reverse to have the same orther that in the strem graph
 for(let genre of GENRES.slice().reverse())
 {
-
-   // $('#genre').append("<input  name=\""+genre+"\" type=\"checkbox\" id=\""+genre+"id\"  checked > <label for=\""+genre+"id\"  style=\"color:"+  COLOR[GENRES.indexOf(genre)]+"\" >"+genre+"</label><br/>");
-    
-    //console.log((15+5+2*genre.length))
+    //build a canvas,with size proportionaly to the name of genre 
     let can =  $("<canvas  width=\""+(15+5+7+10*genre.length)+"\" height=\"40\"></canvas>");
     $('#genre').append(can)
-
      let check =   can[0];
-     
-     check.type = "canvas";
-    
+     //check.type = "canvas";
     //console.log(check)
     let canvas = check.getContext("2d");
     
+    //Draw a circle lije in the map
     canvas.beginPath();
                     canvas.fillStyle = COLOR[GENRES.indexOf(genre)];
                     canvas.arc(20, 20, 15, 0, Math.PI*2);
@@ -74,21 +76,29 @@ for(let genre of GENRES.slice().reverse())
 
                     canvas.fillText(genre, 40, 20);
                     
-                    
+    //add name propriety              
     check.name =genre ;
+    
      can.on("click",
                  function() {
                 console.log(this.name)
                 if (this.checked) {
                     this.checked = false;
+                    //Push the genre in the SELECTEDGENRE if the user click on the circle or texte if we are checked 
                     SELECTEDGENRE.push(this.name);
+                    
+                    // remove opacity if we add the genre
                     can.fadeTo(250,1);
                 } else {
+                   //remove the genre in the SELECTEDGENRE if the user click on the circle or texte and we are not checked 
                     SELECTEDGENRE = SELECTEDGENRE.filter(e => e !== this.name);
                                         this.checked = true;
+                   // set opacity if we remove the genre
                                         can.fadeTo(250,0.25);
                 }
+               // keep SELECTEDGENRE in the right order
                 SELECTEDGENRE = SELECTEDGENRE.sort();
+                // Update all if SELECTEDGENRE is modified
                 fill_stream();
                 updatestream();
                 updatemap();
@@ -98,7 +108,7 @@ for(let genre of GENRES.slice().reverse())
 
 
 
-
+// Return what are the actual data selected buy the user
 function SelectToShow()
 {
     var data = DATA;
@@ -114,12 +124,14 @@ function SelectToShow()
   return  selected;
 }
 
-
+// DO a asycronous sleep 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
+
+// Variable to the automatic timeline 
 let stop = false;
 let isPaused = false;
 let resolvePause = () => {};
@@ -128,6 +140,7 @@ let pauseProm = Promise.resolve();
 //var history_button = document.getElementById('history_button');
 //history_button.value = "Start";
 
+// When the user click on the start/stop button, start/stop the automatic timeline if needed 
 function history_button_action() {
     if (!in_discover) {
         return discover();
@@ -138,6 +151,7 @@ function history_button_action() {
     }
 }
 
+//Pause the automatic timeline
 function sendPause() {
     if(isPaused)
     {
@@ -148,19 +162,22 @@ function sendPause() {
     //history_button.value = "Continue";
     pauseProm = new Promise(resolve => resolvePause = resolve);
 }
-
+//Continue the automatic timeline
 function sendContinue() {
     isPaused = false;
     //history_button.value = "Pause";
     resolvePause();
 }
+//STOP the automatic timeline
 
  function sendStop() {
     sendContinue();
     stop = true;
 }
 
+// varaible useful, so that the user can't launch twice the the automatic timeline
 var in_discover = false;
+//initiale time to wait bewtenne transition
 var speed = 750;
 async function discover()
 {
@@ -200,20 +217,28 @@ async function discover()
 
 }
 
+
 function sendSpeedUP()
 {
+    //since speed is a time wi divide it to speed up the transition
+
     speed/=2;
 }
 
 function sendSpeedDown()
 {
+        //since speed is a time times by Two to slow to down
+
     speed*=2;
 }
 
+
+//load the data from the json 
 d3.json("../data/data.json", function(data) {
 
  DATA = data;
- //DATA.data =  DATA.data.slice(0,100)
+ 
+ // build map stream graph
 
   fill_stream();
   updatestream();
--- src/js/streamgraph.js
+++ src/js/streamgraph.js
@@ -4,25 +4,34 @@ global d3
 Js Scirpt for Data visualisation Projet : Evolution of Rock Music
 Autor : Benjamin Girard / Paul Feng / Olga Popovych
 
+This file contain all materail to draw the stream graph
+
 
 */
-// STREAM GRAPH
+// STREAM GRAPH Variable
+// Array contenaing the number of sont by year and genre
 var year = undefined;
 
+//D3 stack
 var stack = d3.stack(),
     layers = undefined;
 
+//size of the X and Y axis
 var heightXAxis = 45;
 var weightYAxis = 5;
 
+//size of the SVG at start up
 var width = $("body").width()-40;
 var height = $("body").height()*0.25;
 
+
+//Build the svg
 var svg = d3.select('#streamgraphSVG')
          .append("svg").attr('class', 'svgstream').attr("width", width)
           .attr("height", height);
 
 
+//The axis and scale
 var xScale = d3.scaleLinear().domain([FIRSTYEAR, LASTYEAR ]);
 var yScale = d3.scaleLinear();
 var xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d")).ticks((LASTYEAR-FIRSTYEAR)/1.0);
@@ -32,18 +41,18 @@ var yAxis = d3.axisLeft(yScale);
 svg.append('g').classed('x axis', true);
 svg.append('g').classed('y axis', true);
 
-
+//Area to draw the stream graph
 var area = d3.area()
     .x(function(d, i) { return xScale(i+FIRSTYEAR); })
     .y0(function(d) { return yScale(d[0]); })
     .y1(function(d) { return yScale(d[1]); });
 
-
+//the brush on the timeline
 var brush = d3.brushX()
     .on("brush end", brushed);
 
 
-
+//Select the tooltip to show number of song by year/genre on the stremgraph 
 var tooltip  =  d3.select(".tooltip");
 
 svg.append("g")
@@ -55,22 +64,23 @@ svg.append("g")
 function brushed()
 {
     
-
   if (!d3.event.sourceEvent) return; // Only transition after input.
     //console.log(d3.event)
   //if(d3.event.type != "end") return;
 
     console.log(d3.event);//.sourceEvent)
 
+    // if user do not a have selection : jsute  click on the streamgraph 
     if( d3.event.selection == null /*|| d3.event.sourceEvent.type=="mouseup"*/) // if no sleection select only one year
     {
+        // we only slect one year
         var dest = xScale.invert(d3.event.sourceEvent.layerX-weightYAxis);
         YEARSTART = Math.floor(dest);
         YEAREND = Math.floor(dest);
     }
     else
     {
-
+        // if we have a selection we roud to the nearrest years
         var s = d3.event.selection;
         s[0] -=  weightYAxis;
         s[1] -=  weightYAxis;
@@ -80,16 +90,17 @@ function brushed()
       YEAREND = Math.round(s[1]);
     }
 
+  // In case of bad selection
   if(YEAREND-YEARSTART<1)
   YEAREND =YEARSTART+1;
 
+
+//finlay we update the map to the slection
   
   updatemap();
 
-    //console.log([(YEARSTART),(YEAREND) ])
 
-    //console.log([xScale(YEARSTART),xScale(YEAREND) ])
-
+  // We only force the brush to take rouded value if user quit the brush slection
   if(d3.event.type != "end") return;
   d3.select(this).transition().call(d3.event.target.move, [weightYAxis+xScale(YEARSTART),weightYAxis+xScale(YEAREND) ] );
 
@@ -104,11 +115,13 @@ function stackMin(layer) {
   return d3.min(layer, function(d) { return d[0]; });
 }
 
+//her we fill the sream graph we the data
 function fill_stream()
 {
     var data = DATA;
     var n  = SELECTEDGENRE.length;
     year =  new Array(LASTYEAR-FIRSTYEAR);
+    // first we built the year data with seleted data
 
     for(var element of data.data)
      {
@@ -119,11 +132,12 @@ function fill_stream()
 
             year[element[0]-FIRSTYEAR][SELECTEDGENRE.indexOf(element[6][0])]+=1;
 
-
         }
      }
-      if(year[0] == undefined)
+     if(year[0] == undefined)
             year[0] = new Array(n).fill(0)
+            
+      // Also fill the array with 0 if not set        
      for(var y=1;y< year.length;y++)
      {
          if(year[y] == undefined)
@@ -136,6 +150,7 @@ function fill_stream()
          //for(var x=0;x< GENRES.length;x++)
          //    year[y][x] = year[y-1][x]+year[y][x];
      }
+     //build the D3 stack 
     stack = d3.stack();
     stack.keys(d3.range(SELECTEDGENRE.length)).offset(d3.stackOffsetWiggle);
     layers = stack(year);
@@ -143,35 +158,41 @@ function fill_stream()
 }
 
 
+//update the strem graph when the size of the svg must bu update: ie the windows is resize by example
 function updatestream()
 {
+    //also refrech the map: it cas be useful to avoi glitch
     map.invalidateSize();
-
 
     //console.log("update");
 
+    //calculate the new size
     width = $("#streamgraphSVG").width()-40;
     height =$("#streamgraph").height()-1;
     //console.log(width,height)
 
-
+    //and the new scale
      xScale.range([0, width-weightYAxis]);
-
      yScale.range([height-heightXAxis, 0]);
 
      //xAxis.scale(xScale)
      //yAxis.scale(yScale);
 
+
     svg.attr("width", width).attr("height", height);
 
+    // add or remove data if user have change the selection
     var paths = svg.select(".stream").selectAll(".chemin")
                .data(layers);
     paths.exit().remove();
     var enter = paths.enter().append("path").attr("transform", "translate(" + weightYAxis + "," + 0 + ")").attr('class', 'chemin')  ;
     enter.merge(paths).attr("d", area).attr("fill", function(d,i) { return COLOR[GENRES.indexOf(SELECTEDGENRE[i])] ; });
 
+
+    //Mouse event when the user enter the mouse on one line on the stremgraph
     function MouseEvent(d,i)
     {
+        //Add brithness
         d3.select('#streamgraph').select(".stream").selectAll(".chemin").transition()
                 .duration(250)
                 .attr("stroke", function(d, j) {
@@ -180,15 +201,15 @@ function updatestream()
                 .attr("stroke-width", 2)
             
             //console.log();
+            //calculte what yer the mouse is
             let anner = Math.floor(xScale.invert(d3.event.clientX));
-            
-            
+            //Add the texte to the toolip
             tooltip.html(SELECTEDGENRE[i]+"<br/>"+year[anner-FIRSTYEAR][i]+" songs in the year "+anner);
             
-            let clip = function(a,b,c){return Math.max(a,Math.min(b,c)) ;} 
-            console.log(d3.event)
             
+            let clip = function(a,b,c){return Math.max(a,Math.min(b,c)) ;} //console.log(d3.event)
             
+            // color the tooltip to the color of genre and place it under the cursor 
             tooltip.style("color",(COLOR[GENRES.indexOf(SELECTEDGENRE[i])] ))
                 .style("left",  clip(0+15,d3.event.clientX,width -85) + "px")		
                 .style("top", (d3.event.clientY+5) + "px");	
@@ -196,6 +217,7 @@ function updatestream()
     }
 
 
+    //lisiten to event
     svg.select(".stream").selectAll(".chemin")
         .attr("opacity", 0.95)
         .classed('selected', true)
@@ -203,14 +225,18 @@ function updatestream()
         .on("mousemove", MouseEvent
             )
         .on("mouseout", function() {
+            // uncolor graph on mouseout and hide the tooltip
             d3.select('#streamgraph').select(".stream").selectAll(".chemin").attr('stroke', 'none')
             tooltip.style("left", "-10%")		
                 .style("top", "-10%");
         })
         
     
+    //update axis
     svg.select('.x.axis').call(xAxis);
     svg.select('.y.axis').call(yAxis).selectAll("text").remove();
+    
+    // rotate label of year to print years by years 
      svg.select('.x.axis').attr("transform", "translate(" + weightYAxis + ","+ (height-heightXAxis )+")")
      .selectAll("text").attr("dx", "-1.2em")
             .attr("dy", "+1.6em")
